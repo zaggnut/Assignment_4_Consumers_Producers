46dabe9a435f607832d6cadcf0e32338f796af29
michael <spartan364@gmail.com>
2018-11-18 16:42:27 -0800

    Reworked to use Atomics and C++ threads

--- buffer.h
+++ buffer.h
@@ -4,5 +4,4 @@
 typedef int buffer_item;
 #define BUFFER_SIZE 5 //preprocessor macro, sets the size of our buffer (found in main.cpp)
 
-
 #endif //BUFFER_H
--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
+//build with at least C++11, uses thread::yield() and atomics instead of semaphores
+
+
 #include <string>
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h> //used for sleep(int seconds); //not miliseconds
-#include <pthread.h>
+#include <thread>
+#include <vector>
 #include <mutex>
-#include <semaphore.h>
+#include <condition_variable>
+#include <atomic>
 #include "buffer.h" //contains some info for our buffer
-//include the pthread libary
 
 using namespace std;
 
 ////////////////function declarations////////////////
 int insert_item(buffer_item item);  //inserts item into buffer, used by Producers
 int remove_item(buffer_item *item); //removes item from buffer, used by Consumers
-void *producer(void *param);         //A producer thread will run this function
-void *consumer(void *param);         //A consumer thread will run this function
+void *producer();                   //A producer thread will run this function
+void *consumer();                   //A consumer thread will run this function
+void printBuffer();                 //Prints the contents of the buffer
 
 buffer_item buffer[BUFFER_SIZE];
-sem_t Empty;
-sem_t Full;
+atomic_int front;
+atomic_int back;
+atomic_int bufferCount;
 mutex *locker;
 
 /*
@@ -33,38 +39,50 @@ argv[3] ==> the number of consumer threads
 */
 int main(int argc, char *argv[])
 {
+    cout << "Michael Lingo, Shane Laskowski" << endl;
     //Get command line arguments argv[1], argv[2], argv[3]
     //Do error checking for bad arguments here
     srand(time(0));
-    int sleepyTime = stoi(argv[1]);
-    int numProducers = stoi(argv[2]);
-    int numConsumers = stoi(argv[3]);
-
+    int sleepyTime = 0;
+    int numConsumers = 0;
+    int numProducers = 0;
+    try
+    {
+        sleepyTime = stoi(argv[1]);
+        numProducers = stoi(argv[2]);
+        numConsumers = stoi(argv[3]);
+    }
+    catch (exception &e)
+    {
+        cerr << "An invalid argument was entered," << endl
+             << "Three arguments are required:" << endl
+             << "How long the program is to run in seconds, followed by "
+             << "a number of producer threads, followed by "
+             << "a number of consumer threads." << endl;
+        return -1;
+    }
     //Intialize the Buffer with something (random?)
     locker = new mutex();
-    sem_init(&Empty, 0, BUFFER_SIZE);
-    sem_init(&Full, 0, 0);
-    
+    front = 0;
+    back = 0;
+    bufferCount = 0;
+    vector<thread *> ProcuerThreads;
+    vector<thread *> ConsumerThreads;
     for (int i = 0; i < numProducers; i++)
     {
         //Create Producer threads
-        pthread_t thread;
-        pthread_create(&thread, NULL, producer, NULL);
+        ProcuerThreads.push_back(new thread(producer));
     }
-    
+
     for (int i = 0; i < numConsumers; i++)
     {
         //Create Consumer threads
-        pthread_t thread;
-        pthread_create(&thread, NULL, consumer, NULL);
+        ConsumerThreads.push_back(new thread(consumer));
     }
     //Make main sleep for certain amount of time
     sleep(sleepyTime);
     locker->lock();
-    int fullcount, emptycount;
-    sem_getvalue(&Full, &fullcount);
-    sem_getvalue(&Empty, &emptycount);
-    cout << fullcount << emptycount << endl;
+    cout << front << back << bufferCount << endl;
     //Exit properly
     return 0;
 }
@@ -74,14 +92,24 @@ int main(int argc, char *argv[])
 int insert_item(buffer_item itemToAdd)
 {
     //insert the item to the buffer, return 0 if successful or -1 for error condition
-    if (sem_wait(&Empty) != 0)
-        return -1;
-    locker->lock();
-    int index;
-    sem_getvalue(&Full, &index);
-    buffer[index] = itemToAdd;
+    while (true)
+    {
+        if (bufferCount >= BUFFER_SIZE)
+            this_thread::yield();
+        else
+        {
+            locker->lock();
+            if (bufferCount >= BUFFER_SIZE)
+                locker->unlock();
+            else
+                break;
+        }
+    }
+    buffer[back] = itemToAdd;
+    back = (back + 1) % BUFFER_SIZE;
+    bufferCount++;
+    printBuffer();
     locker->unlock();
-    sem_post(&Full);
     return 0;
 }
 
@@ -90,19 +118,29 @@ int remove_item(buffer_item *itemThatWasRemoved)
     //removes an item from the buffer, "placed" in *itemThatWasRemoved
     //the item removed is created on the heap ==> once removed we need to keep a track on it
     //use it in main then delete it.
-    if (sem_wait(&Full) != 0)
-        return -1;
-    locker->lock();
-    int index;
-    sem_getvalue(&Full, &index);
-    *itemThatWasRemoved = buffer[index];
+    while (true)
+    {
+        if (bufferCount <= 0)
+            this_thread::yield();
+        else
+        {
+            locker->lock();
+            if (bufferCount <= 0)
+                locker->unlock();
+            else
+                break;
+        }
+    }
+    *itemThatWasRemoved = buffer[front];
+    front = (front + 1) % BUFFER_SIZE;
+    bufferCount--;
+    printBuffer();
     locker->unlock();
-    sem_post(&Empty);
     //return 0 if successful, else return -1 if error condition
     return 0;
 }
 
-void *producer(void *param)
+void *producer()
 {
     buffer_item item;
 
@@ -112,15 +150,14 @@ void *producer(void *param)
         usleep(rand() % 500);
 
         //generate a random number
-        item = rand();
-        if (insert_item(item)) //***remember, 0 results in false, -1 results in true
+        item = rand() % 300;
+        if (insert_item(item) == -1) //***remember, 0 results in false, -1 results in true
             cout << "Error: Can't add, the buffer is currently full" << endl;
-        else
-            cout << "Producer created and added " << item << " to the buffer" << endl;
+        //else cout << "Producer created and added " << item << " to the buffer" << endl;
     }
 }
 
-void *consumer(void *param)
+void *consumer()
 {
     buffer_item item;
 
@@ -129,9 +166,19 @@ void *consumer(void *param)
         //sleep for random period
         usleep(rand() % 500);
 
-        if (remove_item(&item)) //***remember, 0 results in false, -1 results in true
+        if (remove_item(&item) == -1) //***remember, 0 results in false, -1 results in true
             cout << "Error: Can't remove, the buffer is currently empty" << endl;
-        else
-            cout << "Consumer consumed and took out " << item << " from the buffer" << endl;
+        //else cout << "Consumer consumed and took out " << item << " from the buffer" << endl;
     }
+}
+
+void printBuffer()
+{
+
+    cout << "[ ";
+    for (int i = 0; i < bufferCount; i++)
+    {
+        cout << buffer[(front + i) % BUFFER_SIZE] << " ";
+    }
+    cout << "]" << endl;
 }
